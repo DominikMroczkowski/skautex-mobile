bfbc0efeebc935e5f7d06e470115f4456f217d7e
Dominik Mroczkowski <dominikjanmroczkowski@gmail.com>
2020-12-29 16:45:25 +0100

    Repair book delete button

--- lib/src/routes/home/routes/bookings/components/items/view.dart
+++ lib/src/routes/home/routes/bookings/components/items/view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:skautex_mobile/src/helpers/widgets/stream_list.dart';
 import 'bloc/bloc.dart';
 import 'components/tile/tile.dart';
+import 'package:skautex_mobile/src/routes/home/routes/bookings/bloc/bloc.dart' as bookingBloc;
 
 class View extends StatelessWidget {
 
@@ -11,7 +12,7 @@ class View extends StatelessWidget {
 		return StreamList(
 			itemsWatcher: bloc.itemsWatcher,
 			requestWatcher: bloc.requestWatcher,
-			tile: (booking) => ItemTile(booking: booking)
+			tile: (booking) => ItemTile(booking: booking, update: bookingBloc.Provider.of(context).reloadItems)
 		);
 	}
 }
