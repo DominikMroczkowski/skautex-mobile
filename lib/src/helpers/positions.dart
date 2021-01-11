293d974c323f9307f19ebe87ab9fd3c5828af984
Dominik Mroczkowski <dominikjanmroczkowski@gmail.com>
2021-01-11 17:27:50 +0100

    Change positions for Lech Poznan Academia

--- lib/src/helpers/positions.dart
+++ lib/src/helpers/positions.dart
@@ -1,26 +1,33 @@
 List<String> positions = [
-  "goalkeeper",
-	"side defender",
-  "central defender",
-  "defensive help",
-  "middle help 8",
-  "middle help 10",
-  "side help 10",
-  "attacker"
+  "GOALKEEPER 1",
+	"SIDE DEFENDER 2",
+	"SIDE DEFENDER 3",
+  "CENTRAL DEFENDER 4",
+  "CENTRAL DEFENDER 5",
+  "DEFENSIVE HELP 6",
+  "MIDDLE HELP 8",
+  "MIDDLE HELP 10",
+  "SIDE HELP 7",
+  "SIDE HELP 11",
+  "ATTACKER 9"
 ];
 
 String getPolishPosition(String position) {
 	if (!positions.contains(position))
 		return position;
 	final pl = [
-		'Bramkarz',
-		"Pomoc Boczna",
-		'Pomoc Środkowa',
+		'Bramkarz 1',
+		"Pomoc Boczna 2",
+		"Pomoc Boczna 3",
+		'Pomoc Środkowa 4',
+		'Pomoc Środkowa 5',
+		'Pomoc Defensywna 6',
 		'Obrona Boczna',
 		'Środkowa Pomoc 8',
 		'Środkowy Pomoc 10',
+		'Boczna Pomoc 7',
 		'Boczna Pomoc 10',
-		'Napastnik'
+		'Napastnik 9'
 	];
 	return pl[positions.indexOf(position)];
 }
